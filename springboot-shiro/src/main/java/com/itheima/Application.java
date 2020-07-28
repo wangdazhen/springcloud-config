29d36f610f663b449027355fad70f3b303648542
wangdazhen <15886729365@163.com>
2020-07-28 14:46:32 +0800

    [测试]

---
+++ springboot-shiro/src/main/java/com/itheima/Application.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动类
+ * @author lenovo
+ *
+ */
+@SpringBootApplication
+@MapperScan("com.itheima.mapper")
+//作用：指定要变成实现类的接口所在的包，然后包下面的所有接口在编译之后都会生成相应的实现类
+//添加@MapperScan(“com.test.dao”)注解以后，com.test.dao包下面的接口类，在编译之后都会生成相应的实现类
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
