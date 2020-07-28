b436427df54ea91779977b3acfca4ce53b745006
wangdazhen <15886729365@163.com>
2020-07-28 16:02:54 +0800

    11

--- springboot-shiro/src/main/java/com/itheima/Application.java
+++ springboot-shiro/src/main/java/com/itheima/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * SpringBoot启动类2
+ * SpringBoot启动类22
  * @author lenovo
  *
  */
